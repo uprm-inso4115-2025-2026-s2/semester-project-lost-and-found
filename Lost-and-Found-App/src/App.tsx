856b7fd21f82e72879623f55591ef3da60cbde07
FabianAcevedo1 <fabian.acevedo5@upr.edu>
2026-03-14 10:19:41 -0400

    Some modification to the existing UI code in beta

--- Lost-and-Found-App/src/App.tsx
+++ Lost-and-Found-App/src/App.tsx
@@ -1,7 +1,5 @@
-import AppDefault from "./AppDefault";
 import AppUITest from "./AppUITest";
 
 export default function App() {
-  const useUiTest = import.meta.env.VITE_UI_TEST === "true";
-  return useUiTest ? <AppUITest /> : <AppDefault />;
+  return <AppUITest />;
 }
